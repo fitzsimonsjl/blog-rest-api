a13fdf7c76ec55db4539dc7b07351fc2824de228
Jack Fitzsimons <fitzsimonsjal@gmail.com>
2020-03-18 17:51:06 +0100

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+5.times do 
+	post = Post.create(
+		title: Faker::Coffee.blend_name,
+		body: Faker::Coffee.notes )
+	post.comments.create(name: Faker::Book.author, body: Faker::Lorem.paragraph)
+end
