5f5dff0dc0b35f28b772858521e4c98b0105193d
Jack Fitzsimons <fitzsimonsjal@gmail.com>
2020-03-20 17:09:56 +0100

    added Factory Girl gem

--- spec/controllers/api/v1/posts_controller_spec.rb
+++ spec/controllers/api/v1/posts_controller_spec.rb
@@ -2,18 +2,47 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::PostsController do
 
-  describe "GET #index" do
+  describe 'GET #index' do
     before do
       get :index
     end
   end
 
-    it "returns http success" do
+    it 'returns http success on #index' do
       expect(response).to have_http_status(:success)
     end
 
-    it "JSON body response contains expected post attributes" do
-      json_response = JSON.parse(response.body)
-      expect(hash_body.keys).to match_array([:id, :title, :body])
+    it 'JSON body response contains expected post attributes for #index' do
+      have_attributes([:id, :title, :body])
     end
+end
+
+  RSpec.describe Api::V1::PostsController do
+
+    describe 'GET #show' do
+      before do
+        get :show
+      end
+    end
+
+    it 'returns http success on #show' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'JSON body response contains individual post attributes for #show' do
+      have_attributes([:id, :title, :body])
+    end
+  end
+
+   RSpec.describe Api::V1::PostsController do
+
+    describe 'GET #create' do
+      before do
+        get :create
+      end
+    end
+
+    it 'creates a blog post' do
+      post = Post.build(title: "Post title for Rspec Testing", body: "Post body for RSpect Testing")
+      end
   end
