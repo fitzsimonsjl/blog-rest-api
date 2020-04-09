304cb91b150c0b9ee0fbcd6dacf790e5db0d01a3
Jack Fitzsimons <fitzsimonsjal@gmail.com>
2020-04-09 15:54:55 +0200

    updating a few gems and Rspec file

--- Gemfile
+++ Gemfile
@@ -33,11 +33,14 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'rspec-rails'
-  # gem 'factory_girl_rails'
   gem 'factory_bot_rails'
-  # gem 'shoulda-matchers'
+  gem 'pry-rails'
 end
+
+group :test do
+  gem 'rspec-rails'
+  gem 'shoulda-matchers'
+  end
 
 group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,17 @@ module TriangleBlogApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Adding some config for RSpec tests
+    config.generators do |g|
+      g.test_framework :rspec,
+      :fixtures => false,
+      :view_specs => false,
+      :helper_specs => false,
+      :routing_specs => false,
+      :controller_specs => true,
+      :request_specs => false
+    g.fixture_replacement :factory_bot, :dir => "spec/factories"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -34,4 +45,5 @@ module TriangleBlogApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
+end
 end
