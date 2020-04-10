5fd2b0f019ff9ff736665af3affdf709b1ad15f1
Jack Fitzsimons <fitzsimonsjal@gmail.com>
2020-04-10 20:41:53 +0200

    comment controller updated for requests

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -20,7 +20,7 @@ module Api
       @comment = Comment.new(comment_params)
 
       if @comment.save
-        render json: @comment, status: :created, location: @comment
+        render json: { status: 'Success', message: 'Comment has been saved', data: @comment }, status: :ok
       else
         render json: @comment.errors, status: :unprocessable_entity
       end
@@ -28,8 +28,9 @@ module Api
 
     # PATCH/PUT /comments/1
     def update
-      if @comment.update(comment_params)
-        render json: @comment
+      @comment = Comment.find(params[:id])
+      if @comment.update_attributes(comment_params)
+        render json: {status: 'Success', message: 'Comment has been updated', data: @comment}, status: :ok
       else
         render json: @comment.errors, status: :unprocessable_entity
       end
@@ -38,6 +39,7 @@ module Api
     # DELETE /comments/1
     def destroy
       @comment.destroy
+      render json: {status: 'Success', message: 'Comment has been deleted', data: @comment}, status: :ok
     end
 
     private
